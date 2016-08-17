dc9bc7631881acf7aa423b6d091bb53fe3cf5770
Freek Van der Herten <freek@spatie.be>
2016-08-17 09:02:32 -0400

    Applied fixes from StyleCI

--- tests/SitemapGeneratorTest.php
+++ tests/SitemapGeneratorTest.php
@@ -4,10 +4,8 @@ namespace Spatie\Sitemap\Test;
 
 use Spatie\Sitemap\SitemapGenerator;
 
-
 class SitemapGeneratorTest extends TestCase
 {
-
     public function it_crawls()
     {
         $sitemapGenerator = SitemapGenerator::create('https://spatie.be')
