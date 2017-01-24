c9bee6f7dbe93aae0f6effefdddb23ea70e73529
Rodrigo Zani <rodrigo.zhs@gmail.com>
2017-01-24 15:52:15 -0200

    Prevent parse error
    Parse error: syntax error, unexpected 'version' (T_STRING)

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,4 +1,4 @@
-<?xml version="1.0" encoding="UTF-8"?>
+<?= '<'.'?'.'xml version="1.0" encoding="UTF-8"?>'."\n" ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 @foreach($tags as $tag)
     @include('laravel-sitemap::' . $tag->getType())
