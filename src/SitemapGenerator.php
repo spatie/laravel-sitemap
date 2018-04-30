aa3ff6b9934ba212189e0933e7aa3e901a257b26
Freek Van der Herten <freek@spatie.be>
2018-04-30 11:45:36 +0000

    Apply fixes from StyleCI

--- src/SitemapGenerator.php
+++ src/SitemapGenerator.php
@@ -3,10 +3,10 @@
 namespace Spatie\Sitemap;
 
 use GuzzleHttp\Psr7\Uri;
-use Illuminate\Support\Collection;
 use Spatie\Crawler\Crawler;
 use Spatie\Sitemap\Tags\Url;
 use Spatie\Crawler\CrawlProfile;
+use Illuminate\Support\Collection;
 use Psr\Http\Message\UriInterface;
 use Spatie\Sitemap\Crawler\Profile;
 use Spatie\Sitemap\Crawler\Observer;
