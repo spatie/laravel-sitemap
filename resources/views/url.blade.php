cdd5426312fb645868577183ce53f66e3db732e5
Dan <dpock32509@gmail.com>
2018-07-23 09:13:16 -0400

    Adjust template format to improve render output (#185)
    Due to the way blade directives work indentation consistency of rendered content can get wonky. Normally this shouldn't be a big deal, browsers don't care about HTML indentation for rendering pages. However when you're using XML the use case for human readability is slightly more common.
    
    In an effort to support and improve Human Readability this change adjusts the blade directives as necessary to improve indentation consistency with Sitemap XML structures.

--- resources/views/url.blade.php
+++ resources/views/url.blade.php
@@ -2,22 +2,18 @@
     @if (! empty($tag->url))
     <loc>{{ $tag->url }}</loc>
     @endif
-
-    @if (count($tag->alternates))
-    @foreach ($tag->alternates as $alternate)
+@if (count($tag->alternates))
+@foreach ($tag->alternates as $alternate)
     <xhtml:link rel="alternate" hreflang="{{ $alternate->locale }}" href="{{ $alternate->url }}" />
     @endforeach
-    @endif
-
-    @if (! empty($tag->lastModificationDate))
+@endif
+@if (! empty($tag->lastModificationDate))
     <lastmod>{{ $tag->lastModificationDate->format(DateTime::ATOM) }}</lastmod>
-    @endif
-
+@endif
     @if (! empty($tag->changeFrequency))
     <changefreq>{{ $tag->changeFrequency }}</changefreq>
     @endif
-
-    @if (! empty($tag->priority))
+@if (! empty($tag->priority))
     <priority>{{ $tag->priority }}</priority>
     @endif
 </url>
