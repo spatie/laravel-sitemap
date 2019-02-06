66194b774ebb61c5f96d6b3f801f24217cefccfc
David Bonting <david.bonting@hotmail.com>
2019-02-06 22:53:08 +0100

    Adjusted XML header + number_format .(,) (#228)

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,5 +1,8 @@
 <?= '<'.'?'.'xml version="1.0" encoding="UTF-8"?>'."\n" ?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+        http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
 @foreach($tags as $tag)
     @include('laravel-sitemap::' . $tag->getType())
 @endforeach
