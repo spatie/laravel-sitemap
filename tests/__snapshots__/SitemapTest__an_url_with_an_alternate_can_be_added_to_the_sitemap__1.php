219b727fd52997347e215e553f9d5ae54960ceaa
freek <freek@spatie.be>
2019-02-15 19:39:53 +0100

    update snapshots

--- tests/__snapshots__/SitemapTest__an_url_with_an_alternate_can_be_added_to_the_sitemap__1.php
+++ tests/__snapshots__/SitemapTest__an_url_with_an_alternate_can_be_added_to_the_sitemap__1.php
@@ -1,8 +1,7 @@
 <?php return '<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
     <url>
-    
-    <loc>http://localhost/home</loc>
+        <loc>http://localhost/home</loc>
         <xhtml:link rel="alternate" hreflang="nl" href="http://localhost/thuis" />
         <xhtml:link rel="alternate" hreflang="fr" href="http://localhost/maison" />
         <lastmod>2016-01-01T00:00:00+00:00</lastmod>
