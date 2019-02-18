0c639816e39b0d9320b925dd75c0f322a01c1322
Freek Van der Herten <freek@spatie.be>
2019-02-18 09:58:17 +0100

    Apply fixes from StyleCI (#241)

--- tests/__snapshots__/SitemapTest__an_url_with_an_alternate_can_be_added_to_the_sitemap__1.php
+++ tests/__snapshots__/SitemapTest__an_url_with_an_alternate_can_be_added_to_the_sitemap__1.php
@@ -1,4 +1,6 @@
-<?php return '<?xml version="1.0" encoding="UTF-8"?>
+<?php
+
+return '<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
     <url>
         <loc>http://localhost/home</loc>
