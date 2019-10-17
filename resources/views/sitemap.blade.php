c99e8e779075ac29fcf4be9f541d4a0a94ddfbbf
Hasan TINGIR <hasantngr.53@gmail.com>
2019-10-17 18:56:21 +0300

    travix-ci fix

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,8 +1,6 @@
 <?= '<'.'?'.'xml version="1.0" encoding="UTF-8"?>'."\n"; ?>
-<urlset <?= $namespaces ?> >
-
+<urlset <?=$namespaces?>>
 @foreach($tags as $tag)
     @include('laravel-sitemap::' . $tag->getType())
 @endforeach
-
 </urlset>
