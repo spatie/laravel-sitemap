4656868c77f12799aa0863546def7f185cb0757a
Bhavesh11052001 <72150158+Bhavesh11052001@users.noreply.github.com>
2020-10-02 13:08:32 +0530

    Update Alternate.php

--- src/Tags/Alternate.php
+++ src/Tags/Alternate.php
@@ -6,22 +6,18 @@ class Alternate
 {
     /** @var string */
     public $locale;
-
     /** @var string */
     public $url;
-
     public static function create(string $url, string $locale = ''): self
     {
         return new static($url, $locale);
     }
-
     public function __construct(string $url, $locale = '')
     {
         $this->setUrl($url);
 
         $this->setLocale($locale);
     }
-
     /**
      * @param string $locale
      *
@@ -30,10 +26,8 @@ class Alternate
     public function setLocale(string $locale = '')
     {
         $this->locale = $locale;
-
-        return $this;
+            return $this;
     }
-
     /**
      * @param string $url
      *
@@ -42,7 +36,6 @@ class Alternate
     public function setUrl(string $url = '')
     {
         $this->url = $url;
-
         return $this;
     }
 }
