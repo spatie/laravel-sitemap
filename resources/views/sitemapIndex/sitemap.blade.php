68409fa3dce420487e8e73561bd68f12a3addef1
Florian Müllner <flo-m@gmx.at>
2022-06-05 18:31:17 +0200

    remove gap in sitemapIndex output

--- resources/views/sitemapIndex/sitemap.blade.php
+++ resources/views/sitemapIndex/sitemap.blade.php
@@ -2,7 +2,6 @@
     @if (! empty($tag->url))
     <loc>{{ url($tag->url) }}</loc>
     @endif
-
     @if (! empty($tag->lastModificationDate))
     <lastmod>{{ $tag->lastModificationDate->format(DateTime::ATOM) }}</lastmod>
     @endif
