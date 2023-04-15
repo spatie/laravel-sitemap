25f44eadf9d38fb1b3ad39c778144c889729953a
Erfan Sahafnejad <erfan.sahaf@gmail.com>
2023-04-15 16:18:24 +0330

    fix: remove url from balde file

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -1,12 +1,12 @@
 <video:video>
-    <video:thumbnail_loc>{{ url($video->thumbnailLoc) }}</video:thumbnail_loc>
-    <video:title>{{ url($video->title) }}</video:title>
-    <video:description>{{ url($video->description) }}</video:description>
+    <video:thumbnail_loc>{{ $video->thumbnailLoc }}</video:thumbnail_loc>
+    <video:title>{{ $video->title }}</video:title>
+    <video:description>{{ $video->description }}</video:description>
 @if ($video->contentLoc)
-    <video:content_loc>{{ url($video->contentLoc) }}</video:content_loc>
+    <video:content_loc>{{ $video->contentLoc }}</video:content_loc>
 @endif
 @if ($video->playerLoc)
-    <video:player_loc>{{ url($video->playerLoc) }}</video:player_loc>
+    <video:player_loc>{{ $video->playerLoc }}</video:player_loc>
 @endif
 @if ($video->platforms && count($video->platforms) > 0)
     <video:platform relationship="allow">{{ implode($video->playerLoc, " ") }}</video:platform>
