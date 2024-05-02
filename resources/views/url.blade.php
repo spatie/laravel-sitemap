cfa3184177a680d06b39cb53848aadceaa9d8ab4
Hamid Shariati <hmdshariati@aol.com>
2024-05-02 13:15:35 +0330

    remove deprecated tgas from url blade (#545)
    Co-authored-by: hmdshariati <h.shariati@drdr.doctor>

--- resources/views/url.blade.php
+++ resources/views/url.blade.php
@@ -10,10 +10,7 @@
 @if (! empty($tag->lastModificationDate))
     <lastmod>{{ $tag->lastModificationDate->format(DateTime::ATOM) }}</lastmod>
 @endif
-    @if (! empty($tag->changeFrequency))
-    <changefreq>{{ $tag->changeFrequency }}</changefreq>
-    @endif
-    <priority>{{ number_format($tag->priority,1) }}</priority>
+
     @each('sitemap::image', $tag->images, 'image')
     @each('sitemap::video', $tag->videos, 'video')
     @each('sitemap::news', $tag->news, 'news')
