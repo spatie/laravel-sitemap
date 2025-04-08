93226235f515b5f6490b1648f0de9362dd006b7c
Lukas <kosha.industry@gmail.com>
2025-04-09 00:00:05 +0500

    check for null

--- resources/views/url.blade.php
+++ resources/views/url.blade.php
@@ -7,13 +7,13 @@
     <xhtml:link rel="alternate" hreflang="{{ $alternate->locale }}" href="{{ url($alternate->url) }}" />
 @endforeach
 @endif
-@if (! empty($tag->lastModificationDate))
+@if (! is_null($tag->lastModificationDate))
     <lastmod>{{ $tag->lastModificationDate->format(DateTime::ATOM) }}</lastmod>
 @endif
-@if (! empty($tag->changeFrequency))
+@if (! is_null($tag->changeFrequency))
     <changefreq>{{ $tag->changeFrequency }}</changefreq>
 @endif
-@if (! empty($tag->priority))
+@if (! is_null($tag->priority))
     <priority>{{ number_format($tag->priority, 1) }}</priority>
 @endif
     @each('sitemap::image', $tag->images, 'image')
