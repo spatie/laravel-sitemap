819107eab8391b3c918286478d6b608d52bc9899
Lukas <kosha.industry@gmail.com>
2025-04-08 23:51:18 +0500

    fix align

--- resources/views/url.blade.php
+++ resources/views/url.blade.php
@@ -5,7 +5,7 @@
 @if (count($tag->alternates))
 @foreach ($tag->alternates as $alternate)
     <xhtml:link rel="alternate" hreflang="{{ $alternate->locale }}" href="{{ url($alternate->url) }}" />
-    @endforeach
+@endforeach
 @endif
 @if (! empty($tag->lastModificationDate))
     <lastmod>{{ $tag->lastModificationDate->format(DateTime::ATOM) }}</lastmod>
