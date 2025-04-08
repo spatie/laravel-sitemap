4cc599c83a7dd952d823d436976be3a47c973814
Lukas <kosha.industry@gmail.com>
2025-04-08 23:51:06 +0500

    render priority

--- resources/views/url.blade.php
+++ resources/views/url.blade.php
@@ -13,6 +13,9 @@
 @if (! empty($tag->changeFrequency))
     <changefreq>{{ $tag->changeFrequency }}</changefreq>
 @endif
+@if (! empty($tag->priority))
+    <priority>{{ number_format($tag->priority, 1) }}</priority>
+@endif
     @each('sitemap::image', $tag->images, 'image')
     @each('sitemap::video', $tag->videos, 'video')
     @each('sitemap::news', $tag->news, 'news')
