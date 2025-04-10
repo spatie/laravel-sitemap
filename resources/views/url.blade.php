506b2acdd350c7ff868a7711b4f30e486b20e9b0
Lukas Pierce <kosha.industry@gmail.com>
2025-04-10 17:13:41 +0500

    Render <changefreq> and <priority> (#572)
    * Add changefreq
    
    Spatie\Sitemap\Tags\Url provides setChangeFrequency()
    
    but it not renders in xml
    
    * no default value for $changeFrequency
    
    * add missed prop
    
    * add missed prop
    
    * ignore .phpunit.cache
    
    * ignore .idea
    
    * delete .phpunit.cache
    
    * optional props
    
    * render priority
    
    * fix align
    
    * add priority
    
    * initial null values
    
    * check for null
    
    * fix alignment
    
    * add priority
    
    * add priority

--- resources/views/url.blade.php
+++ resources/views/url.blade.php
@@ -1,14 +1,20 @@
 <url>
-    @if (! empty($tag->url))
+@if (! empty($tag->url))
     <loc>{{ url($tag->url) }}</loc>
-    @endif
+@endif
 @if (count($tag->alternates))
 @foreach ($tag->alternates as $alternate)
     <xhtml:link rel="alternate" hreflang="{{ $alternate->locale }}" href="{{ url($alternate->url) }}" />
-    @endforeach
+@endforeach
 @endif
-@if (! empty($tag->lastModificationDate))
+@if (! is_null($tag->lastModificationDate))
     <lastmod>{{ $tag->lastModificationDate->format(DateTime::ATOM) }}</lastmod>
+@endif
+@if (! is_null($tag->changeFrequency))
+    <changefreq>{{ $tag->changeFrequency }}</changefreq>
+@endif
+@if (! is_null($tag->priority))
+    <priority>{{ number_format($tag->priority, 1) }}</priority>
 @endif
     @each('sitemap::image', $tag->images, 'image')
     @each('sitemap::video', $tag->videos, 'video')
